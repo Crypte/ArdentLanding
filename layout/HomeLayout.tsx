bbea107a0742850e46994643996a5d78bb703461
Crypte <github.virus521@passinbox.com>
2025-09-13 17:38:03 +0200

    Working

--- layout/HomeLayout.tsx
+++ layout/HomeLayout.tsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router-dom"
 
 export default function HomeLayout() {
     return (
-        <main className={'h-screen w-screen flex flex-col items-center justify-center bg-gradient-to-br from-bg-tertiary/30 to-tertiary-foreground/50'}>
+        <main className={'h-screen w-screen flex flex-col items-center justify-center bg-gradient-to-br from-tertiary-foreground/05 to-tertiary-foreground/40'}>
             <div className={'container'}>
             <Outlet />
             </div>
--- src/components/home/HeroCard.tsx
+++ src/components/home/HeroCard.tsx
@@ -1,5 +1,4 @@
 import {Badge} from "@/components/ui/badge.tsx";
-import RotateHeroText from "@/components/home/RotateHeroText.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {Link} from "react-router-dom";
 import { useStats } from "@/hooks/useStats";
@@ -22,24 +21,18 @@ export default function HeroCard() {
                 </CardHeader>
                 <CardContent className=" flex flex-col item-center space-y-5 w-full">
                 <div className={'space-y-4 text-center'}>
-                <RotateHeroText />
+                <h1 className={'font-display text-4xl font-bold italic drop-shadow-sm md:text-7xl'}>L'ignorance n'est plus une option</h1>
                 <p className="text-muted-foreground font-light">
                     Accéder à la plus grande base de données de savoir
                 </p>
                 </div>
                 {/* Stats cards */}
-                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                <div className="grid grid-cols-3 gap-4">
                     <div className="border rounded-lg p-4 text-center bg-tertiary">
                         <div className="text-2xl font-bold text-tertiary-foreground">
                             {loading || error ? "..." : stats?.total_published_resources}
                         </div>
                         <div className="text-xs text-muted-foreground">Ressources</div>
-                    </div>
-                    <div className=" border rounded-lg p-4 text-center bg-tertiary">
-                        <div className="text-2xl font-bold text-tertiary-foreground">
-                            {loading || error ? "..." : stats?.total_users_verified}
-                        </div>
-                        <div className="text-xs text-muted-foreground">Inscrits</div>
                     </div>
                     <div className="bg-tertiary border rounded-lg p-4 text-center">
                         <div className="text-2xl font-bold text-tertiary-foreground">
